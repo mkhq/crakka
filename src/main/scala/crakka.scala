6849698f3cf64cc949c43de46108d7d159a83489
mikael <mikael@nano.(none)>
2009-11-24 10:11:31 +0100

    - added actor id

--- src/main/scala/crakka.scala
+++ src/main/scala/crakka.scala
@@ -4,9 +4,42 @@ import se.scalablesolutions.akka.actor.Actor
 import se.scalablesolutions.akka.nio.{RemoteServer}
 import se.scalablesolutions.akka.util.Logging
 import se.scalablesolutions.akka.serialization._
+import sbinary.DefaultProtocol._
+
+case class ActorId(hostname:String, port:Int, Id:Int) extends Serializable.SBinary[ActorId] {
+  implicit object ActorIDFormat extends Format[ActorId] {
+	 def reads(in: Input) = ActorId(
+		read[String](in),
+		read[Int](in),
+		read[Int](in))
+
+	 def writes(out: Output, value: ActorId) = {
+		write[String](out, value.hostname)
+		write[Int](out, value.port)
+		write[Int](out, value.Id)
+	 }
+  }
+
+  def fromBytes(bytes: Array[Byte]) = fromByteArray[ActorId](bytes)
+  def toBytes: Array[Byte] = toByteArray(this)
+}
+
+case class Event(from:ActorId, msg:AnyRef) extends Serializable.SBinary[Event] {
+  implicit object EventFormat extends Format[Event] {
+	 def reads(in: Input) = Event(
+		read[ActorId](in),
+		read[AnyRef](in))
+	 def writes(out: Output, value: Event) = {
+		write[ActorId](out, value.from)
+		write[AnyRef](out, value.msg)
+	 }
+  }
+
+  def fromBytes(bytes: Array[Byte]) = fromByteArray[Event](bytes)
+  def toBytes: Array[Byte] = toByteArray(this)
+}
 
 case class Ping() extends Serializable.SBinary[Ping] {
-import sbinary.DefaultProtocol._
   
   implicit object PingFormat extends Format[Ping] {
 	 def reads(in: Input) = Ping()
